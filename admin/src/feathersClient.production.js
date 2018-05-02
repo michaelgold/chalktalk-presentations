2446822feb8d5037eb41e96c308980dadd11fda7
Michael Gold <goldmichael@gmail.com>
2018-05-02 09:31:37 -0400

    added feathersclient with production values

---
+++ admin/src/feathersClient.production.js
@@ -0,0 +1,13 @@
+import feathers from 'feathers-client';
+import decodeJwt from 'jwt-decode';
+
+const host = 'http://104.236.33.63:3030';
+
+export default feathers()
+    .configure(feathers.hooks())
+    .configure(feathers.rest(host).fetch(window.fetch.bind(window)))
+    .configure(feathers.authentication({ jwtStrategy: 'jwt', storage: window.localStorage }));
+
+//export const UserID = decodeJwt(localStorage.getItem('feathers-jwt')).id;
+
+export const UserID = (localStorage.getItem('feathers-jwt') == null) ? "" : decodeJwt(localStorage.getItem('feathers-jwt')).id;
